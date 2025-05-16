9820d0484fc3a0f89051242dd50eac88444e421b
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2025-05-16 15:35:54 +0200

    Use data storage manager to save and get preferences data

--- Assets/Scripts/Menu/GameSettings.cs
+++ Assets/Scripts/Menu/GameSettings.cs
@@ -1,3 +1,4 @@
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -10,49 +11,58 @@ public class GameSettings : MonoBehaviour
     public InputField ageInputField;
     public Text ageDisplayText;
 
+    public ServiceLocator serviceLocator;
+    private DataStorageManager _storage;
+
     private const string VolumeKey = "game_volume";
     private const string AgeKey = "player_age";
 
     void Start()
     {
-        Debug.Log("GameSettings script started.");
-        float savedVolume = PlayerPrefs.GetFloat(VolumeKey, 1f);
-        volumeSlider.value = savedVolume;
-        Debug.Log("Volume slider value set to: " + savedVolume);
-        //audioSource.volume = savedVolume;
-        //Debug.Log("Audio source volume set to: " + savedVolume);
+        if (serviceLocator == null)
+        {
+            Debug.LogError("ServiceLocator is not assigned.");
+            return;
+        }
+        _storage = serviceLocator.GetDataStorageManager();
 
-        int savedAge = PlayerPrefs.GetInt(AgeKey, 18);
-        Debug.Log("Saved age from PlayerPrefs: " + savedAge);
+        float savedVolume = _storage.LoadFloat(VolumeKey, 1f);
+        volumeSlider.value = savedVolume;
+        //audioSource.volume = savedVolume;
+
+        int savedAge = _storage.LoadInt(AgeKey, 18);
         ageInputField.text = savedAge.ToString();
         ageDisplayText.text = savedAge.ToString();
-        Debug.Log("Age input field and display text set to: " + savedAge);
 
         volumeSlider.onValueChanged.AddListener(UpdateVolume);
         ageInputField.onEndEdit.AddListener(UpdateAge);
-        Debug.Log("Listeners added to volume slider and age input field.");
     }
 
     public void UpdateVolume(float newVolume)
     {
-        Debug.Log("Volume updated to: " + newVolume);
         //audioSource.volume = newVolume;
         PlayerPrefs.SetFloat(VolumeKey, newVolume);
         PlayerPrefs.Save();
-        Debug.Log("Volume saved to PlayerPrefs: " + newVolume);
     }
 
     public void UpdateAge(string input)
     {
-        Debug.Log("Age input received: " + input);
         if (int.TryParse(input, out int newAge))
         {
-            Debug.Log("Parsed age: " + newAge);
             ageDisplayText.text = newAge.ToString();
             PlayerPrefs.SetInt(AgeKey, newAge);
             PlayerPrefs.Save();
-            Debug.Log("Age saved to PlayerPrefs: " + newAge);
         }
     }
 
+    public void ResetSettings()
+    {
+        volumeSlider.value = 1f;
+        //audioSource.volume = 1f;
+        _storage.SaveFloat(VolumeKey, 1f);
+
+        ageInputField.text = "18";
+        ageDisplayText.text = "Leeftijd: 18";
+        _storage.SaveInt(AgeKey, 18);
+    }
 }
